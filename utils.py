756cbc74abad3a487d30fa0d1f84436a3a6e993b
Arp1988 <adityaranjan.patra@gmail.com>
2023-06-03 09:07:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,51 @@
+from tqdm import tqdm
 
+def GetCorrectPredCount(pPrediction, pLabels):
+  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+# CODE BLOCK 9
+
+def train(model, device, train_loader, optimizer, criterion):
+  model.train()
+  pbar = tqdm(train_loader)
+
+  train_loss = 0
+  correct = 0
+  processed = 0
+
+  for batch_idx, (data, target) in enumerate(pbar):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+
+    # Predict
+    pred = model(data)
+
+    # Calculate loss
+    loss = criterion(pred, target)
+    train_loss+=loss.item()
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+    
+    correct += GetCorrectPredCount(pred, target)
+    processed += len(data)
+
+    pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+  train_acc.append(100*correct/processed)
+  train_losses.append(train_loss/len(train_loader))
+
+def test(model, device, test_loader, criterion):
+    model.eval()
+    
+# CODE BLOCK 11
+
+fig, axs = plt.subplots(2,2,figsize=(15,10))
+axs[0, 0].plot(train_losses)
+axs[0, 0].set_title("Training Loss")
+axs[1, 0].plot(train_acc)
+axs[1, 0].set_title("Training Accuracy")
+axs[0, 1].plot(test_losses)
+axs[0, 1].set_title("Test Loss")
+axs[1, 1].plot(test_acc)
+axs[1, 1].set_title("Test Accuracy")
